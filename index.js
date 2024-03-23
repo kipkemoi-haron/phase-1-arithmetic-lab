8e666011e7f3ae7d3337ef3b757f6a92e9a3d904
kipkemoi Haron <kipkemoiharon4@gmail.com>
2024-03-23 14:07:48 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
-// Write your code here
+// creating variable multiply
+var num1 = 31;
+var num2 = 2;
+var multiply = num1 * num2;
+
+//generate random interger greater than 0
+var random = Math.floor(Math.random() * 100) + 1; 
+
+//variable 'mod' 
+var num3 = 64;
+var num4 = 6;
+var mod = num3 % num4;
+
+//variable 'max' 
+var numbers = [5, 10, 15, 20, 3];
+var max = Math.max(...numbers);
